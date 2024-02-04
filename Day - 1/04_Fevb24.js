022e48cc51a614fde966fc69bd5da2185930c47e
Bhupender singh <sbhupender1010@gmail.com>
2024-02-04 10:36:51 +0000

    Day1

---
+++ Day - 1/04_Fevb24.js
@@ -0,0 +1,10 @@
+const accountPass = 123456
+let accountName = "Rahul"
+var accountState = "Rajasthan"
+accountCity = "Jaipur"
+
+//accountPass = 9876 // cant change value of const
+console.log(accountPass);
+console.table([accountName,accountState,accountCity])
+
+// we dont var because of block and functional scope -- variable declared using var change be changed
