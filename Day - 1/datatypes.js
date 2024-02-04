5d7ca63960e30824e4d867a5ba35d5849f682035
Bhupender singh <sbhupender1010@gmail.com>
2024-02-04 11:06:35 +0000

    datatypes

---
+++ Day - 1/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // to use the newer version of js
+
+//alert(3+3) == use in browser, different syntax for node
+
+//number == 3
+// string == "rahul"
+// bigint
+//boolean -- true/false
+// null == type of null is object -- null means empty -- it is a standalone value
+// undefined - type is undefined
+
+// object
+console.log(typeof null);
+console.log(typeof undefined);
