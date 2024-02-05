28ea1a91f99dd16e4d20a67f543ab0fab1171bca
Bhupender singh <sbhupender1010@gmail.com>
2024-02-05 12:45:22 +0000

    conditions

---
+++ Day - 2/conditions.js
@@ -0,0 +1,6 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// in js conditional operators and equall works different
+// conditional operator converts the null to 0 in number and hence we get true in statement 3
+
