f0e64d4334986db71b421419d709c4bd3c02e230
Bhupender singh <sbhupender1010@gmail.com>
2024-02-05 11:04:05 +0000

    Day 2

---
+++ Day -2/operators.js
@@ -0,0 +1,17 @@
+// console.log('1'+2);
+// console.log('1'+2+3);
+// console.log(1+"2"+3);
+// console.log(1+2+'3');
+// whenever first datatype is string -- whole will be treated as string 
+
+// prefix and postfix
+// let x = 2;
+// let y = x++;
+// console.log(`x is ${x}, y is ${y}`);
+// first x is assigned to y and then incremented
+
+// let x = 2;
+// let y = ++x;
+// console.log(`x is ${x}, y is ${y}`);
+
+// x is increated and then assigned to y
